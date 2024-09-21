204a3ed5aa67c8137395e73da96f260aa397b803
asensiodev <asensiodev@gmail.com>
2024-09-21 14:47:56 +0200

    Add engine model and test

---
+++ MyUnitTest/app/src/test/java/com/asensiodev/myunittest/EngineTest.kt
@@ -0,0 +1,25 @@
+package com.asensiodev.myunittest
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class EngineTest {
+    private val engine = Engine(2000, 189, 15, false)
+
+    @Test
+    fun engine_turnsOn() {
+        engine.turnOn()
+
+        assertEquals(true, engine.isTurnedOn)
+        assertEquals(95, engine.temperature)
+    }
+
+    @Test
+    fun engine_TurnsOff() {
+        engine.turnOn()
+        engine.turnOff()
+
+        assertEquals(false, engine.isTurnedOn)
+        assertEquals(15, engine.temperature)
+    }
+}
