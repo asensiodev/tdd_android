23f97cc998e77461178c4011353cb49a68b7f913
asensiodev <asensiodev@gmail.com>
2024-09-21 18:19:49 +0200

    Add models

--- OutsideInTDDExample/app/src/main/java/com/asensiodev/outsideintddexample/Car.kt
+++ OutsideInTDDExample/app/src/main/java/com/asensiodev/outsideintddexample/Car.kt
@@ -1,11 +1,20 @@
 package com.asensiodev.outsideintddexample
 
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 class Car(
     var fuel: Double,
     val engine: Engine,
 ) {
     fun turnOn() {
-        engine.turnOn()
         fuel -= 0.5
+        CoroutineScope(Dispatchers.Main).launch {
+            engine.turnOn().collect { temperature ->
+                Log.d("COURSE", "Collected engine temperature: $temperature")
+            }
+        }
     }
 }
--- OutsideInTDDExample/app/src/main/java/com/asensiodev/outsideintddexample/Engine.kt
+++ OutsideInTDDExample/app/src/main/java/com/asensiodev/outsideintddexample/Engine.kt
@@ -1,11 +1,32 @@
 package com.asensiodev.outsideintddexample
 
+import android.util.Log
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.time.Duration.Companion.seconds
+
 class Engine(
-    var isTurnedOn: Boolean,
-    var temperature: Int,
+    var isTurnedOn: Boolean = false,
+    var temperature: Int = 15,
 ) {
-    fun turnOn() {
+    suspend fun turnOn(): Flow<Int> {
         isTurnedOn = true
-        temperature = 95
+
+        return flow {
+            delay(2.seconds)
+            temperature = 25
+            emit(temperature)
+
+            delay(2.seconds)
+            temperature = 50
+            emit(temperature)
+
+            delay(2.seconds)
+            temperature = 95
+            emit(temperature)
+
+            Log.d("COURSE", "Engine has turned on")
+        }
     }
 }
