951f2a43a8d37b7fa044206e0adc0effeada7f91
asensiodev <asensiodev@gmail.com>
2024-09-21 18:20:19 +0200

    Add Engine tests

--- OutsideInTDDExample/app/src/test/java/com/asensiodev/outsideintddexample/unittests/EngineShould.kt
+++ OutsideInTDDExample/app/src/test/java/com/asensiodev/outsideintddexample/unittests/EngineShould.kt
@@ -1,24 +1,34 @@
 package com.asensiodev.outsideintddexample.unittests
 
 import com.asensiodev.outsideintddexample.Engine
+import com.asensiodev.outsideintddexample.utils.MainCoroutineScopeRule
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 
 class EngineShould {
     private val engine = Engine(false, 15)
 
-    @Test
-    fun turnOn() {
-        engine.turnOn()
-
-        assertTrue(engine.isTurnedOn)
-    }
+    @get:Rule
+    var coroutineTestRule = MainCoroutineScopeRule()
 
     @Test
-    fun riseTemperatureWhenIsTurnedOn() {
-        engine.turnOn()
+    fun turnOn() =
+        runTest {
+            engine.turnOn()
 
-        assertEquals(95, engine.temperature)
-    }
+            assertTrue(engine.isTurnedOn)
+        }
+
+    @Test
+    fun riseTemperatureGraduallyWhenIsTurnedOn() =
+        runTest {
+            val flow = engine.turnOn()
+            val actual = flow.toList()
+
+            assertEquals(listOf(25, 50, 95), actual)
+        }
 }
