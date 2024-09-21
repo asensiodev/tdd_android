4cab0ad80ce1a79a63e785f12bbb6043cb215d84
asensiodev <asensiodev@gmail.com>
2024-09-21 18:20:10 +0200

    Add Car Feature tests

--- OutsideInTDDExample/app/src/test/java/com/asensiodev/outsideintddexample/acceptancetests/CarFeature.kt
+++ OutsideInTDDExample/app/src/test/java/com/asensiodev/outsideintddexample/acceptancetests/CarFeature.kt
@@ -2,26 +2,45 @@ package com.asensiodev.outsideintddexample.acceptancetests
 
 import com.asensiodev.outsideintddexample.Car
 import com.asensiodev.outsideintddexample.Engine
+import com.asensiodev.outsideintddexample.utils.MainCoroutineScopeRule
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 
 class CarFeature {
     private val engine = Engine(false, 15)
     private val car = Car(6.0, engine)
 
-    @Test
-    fun carIsLoosingFuelWhenItTurnsOn() {
-        car.turnOn()
-
-        assertEquals(5.5, car.fuel)
-    }
+    @get:Rule
+    var coroutineTestRule = MainCoroutineScopeRule()
 
     @Test
-    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
-        car.turnOn()
+    fun carIsLoosingFuelWhenItTurnsOn() =
+        runTest {
+            car.turnOn()
 
-        assertEquals(95, car.engine.temperature)
-        assertTrue(car.engine.isTurnedOn)
-    }
+            assertEquals(5.5, car.fuel)
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun carIsTurningOnItsEngineAndIncreasesTheTemperatureGradually() =
+        runTest {
+            car.turnOn()
+
+            advanceTimeBy(2001)
+            assertEquals(25, car.engine.temperature)
+
+            advanceTimeBy(2001)
+            assertEquals(50, car.engine.temperature)
+
+            advanceTimeBy(2001)
+            assertEquals(95, car.engine.temperature)
+
+            assertTrue(car.engine.isTurnedOn)
+        }
 }
